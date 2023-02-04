036fc2e2299eb115d5ec6c757ec979da0835455f
waterproofpatch <waterproofpatch@gmail.com>
2023-02-03 23:31:36 -0500

    base services

---
+++ frontend/app/src/app/services/base.service.ts
@@ -0,0 +1,22 @@
+import { HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BaseComponent } from '../components/base/base.component';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BaseService extends BaseComponent {
+  public httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    }),
+  };
+  constructor() {
+    super();
+  }
+  getUrlBase(): string {
+    return environment.apiUrlBase;
+  }
+}
