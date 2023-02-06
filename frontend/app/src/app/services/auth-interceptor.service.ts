91f4a704ceb56a5c216cf34948f3640d8b12947c
waterproofpatch <waterproofpatch@gmail.com>
2023-02-05 20:56:03 -0500

    registration from the frontend

---
+++ frontend/app/src/app/services/auth-interceptor.service.ts
@@ -0,0 +1,63 @@
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { Injectable, Injector } from '@angular/core';
+import { HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
+import { AuthenticationService } from './authentication.service';
+import { DialogService } from './dialog.service';
+
+@Injectable()
+export class AuthInterceptorService implements HttpInterceptor {
+  constructor(
+    private injector: Injector,
+    private dialogService: DialogService,
+    private authenticationService: AuthenticationService
+  ) { }
+
+  intercept(req: any, next: any) {
+    const loginService = this.injector.get(AuthenticationService);
+    const authRequest = req.clone({
+      headers: req.headers.append(
+        'Authorization',
+        'Bearer ' + loginService.token
+      ),
+    });
+
+    return next.handle(authRequest).pipe(
+      catchError((error) => {
+        if (error instanceof HttpErrorResponse) {
+          if (error.error instanceof ErrorEvent) {
+            console.error('Error Event');
+          } else {
+            console.log(`error status : ${error.status} ${error.statusText}`);
+            switch (error.status) {
+              case 400:
+                this.dialogService.displayErrorDialog(
+                  'Bad request: ' + error.error.error_message
+                );
+                break;
+              case 401: //login
+                this.dialogService.displayErrorDialog(
+                  '401 - Unauthorized: ' + error.error.error_message
+                );
+                this.authenticationService.logout();
+                break;
+              case 403: //forbidden
+                this.dialogService.displayErrorDialog(
+                  '403 - Forbidden: ' + error.error.error_message
+                );
+                this.authenticationService.logout();
+                break;
+              default:
+                this.dialogService.displayErrorDialog(
+                  'Unknown error ' + error.status
+                );
+            }
+          }
+        } else {
+          console.error('some thing else happened');
+        }
+        return throwError(error);
+      })
+    );
+  }
+}
