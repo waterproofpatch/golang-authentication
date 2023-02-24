ffa62dbcd1db08805a48a4f6d5783c8d885e6549
waterproofpatch <waterproofpatch@gmail.com>
2023-02-24 00:39:17 -0500

    env

--- frontend/app/src/app/services/websocket.service.ts
+++ frontend/app/src/app/services/websocket.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebsocketService {
   private socket: WebSocket;
-  private readonly url: string = 'ws://192.168.1.252:5000/ws';
 
   constructor() {
-    this.socket = new WebSocket(this.url);
+    this.socket = new WebSocket(environment.wsUrl);
   }
 
   public sendMessage(message: string): void {
--- frontend/app/src/environments/environment.ts
+++ frontend/app/src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false,
   apiUrlBase: 'http://localhost:5000',
+  wsUrl: 'ws://localhost:5000/ws',
 };
 
 /*
