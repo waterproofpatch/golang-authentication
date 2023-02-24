472d9c27949b5e6b5b38954e4ea9e387443efdbf
waterproofpatch <waterproofpatch@gmail.com>
2023-02-24 01:14:42 -0500

    name gen

--- frontend/app/src/app/components/chat/chat.component.ts
+++ frontend/app/src/app/components/chat/chat.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { WebsocketService } from 'src/app/services/websocket.service';
 import { Message } from 'src/app/services/websocket.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,14 +12,34 @@ export class ChatComponent {
   title = '';
   message: string = '';
   username: string = 'John Doe';
-  messages: string[] = [];
+  messages: Message[] = [];
 
   constructor(private chatService: WebsocketService) { }
 
   ngOnInit(): void {
-    this.chatService.getMessages().subscribe(message => {
+    this.initUsername()
+    this.chatService.getMessages().pipe(
+      map((message: any) => {
+        let message_json: Message = JSON.parse(message)
+        return { from: message_json.from, content: message_json.content };
+      })
+    ).subscribe((message: Message) => {
       this.messages.push(message);
     });
+  }
+
+  initUsername() {
+    const firstNames = ['Emma', 'Liam', 'Olivia', 'Noah', 'Ava', 'Ethan', 'Isabella', 'Mason', 'Sophia', 'Logan'];
+    const lastNames = ['Smith', 'Johnson', 'Williams', 'Brown', 'Jones', 'Garcia', 'Miller', 'Davis', 'Rodriguez', 'Martinez'];
+
+    function generateUsername(): string {
+      const firstName = firstNames[Math.floor(Math.random() * firstNames.length)];
+      const lastName = lastNames[Math.floor(Math.random() * lastNames.length)];
+      return `${firstName} ${lastName}`;
+    }
+
+    // Example usage:
+    this.username = generateUsername();
   }
 
   sendMessage(): void {
