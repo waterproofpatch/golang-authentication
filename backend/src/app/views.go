770fb6931abc63ed1df762fde865379e7c663498
waterproofpatch <waterproofpatch@gmail.com>
2023-05-07 23:30:08 -0400

    form doesn't disappear on error

--- backend/src/app/views.go
+++ backend/src/app/views.go
@@ -183,6 +183,7 @@ func plants(w http.ResponseWriter, r *http.Request, claims *authentication.JWTDa
 		json.NewEncoder(w).Encode(plants)
 		break
 	case "PUT":
+		fmt.Println("Sleeping...")
 		plantId, err := strconv.Atoi(r.FormValue("id"))
 		var isNewImage = false
 		if err != nil {
