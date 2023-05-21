6f74ac59a4c42343a7642ef720e6b2074b4d2632
waterproofpatch <waterproofpatch@gmail.com>
2023-05-20 21:29:17 -0400

    dont destroy volumes on down;

--- cleanup-containers.sh
+++ cleanup-containers.sh
@@ -1,4 +1,7 @@
-docker-compose down --volumes
+#!/bin/bash
+# Clean up containers and compose services. 
+# docker-compose down --volumes
+docker-compose down
 docker network rm backend_app-net || true
 docker rm golang-template-backend-1 || true
 docker rm golang-template-frontend-1 || true
