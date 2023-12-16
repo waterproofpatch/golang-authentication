491e3947854711b57fb5afd0f2a771541d35af6f
waterproofpatch <waterproofpatch@gmail.com>
2023-12-16 03:10:56 -0500

    space

--- frontend/src/app/services/authentication.service.ts
+++ frontend/src/app/services/authentication.service.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { Observable, Subject, BehaviorSubject, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
 import jwt_decode from 'jwt-decode';
 import { JwtPayload } from 'jwt-decode';
 import { finalize } from 'rxjs/operators';
