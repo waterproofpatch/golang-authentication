bb5dde410a5162cf277417c066b5f25efc953167
waterproofpatch <waterproofpatch@gmail.com>
2023-12-24 22:47:38 -0500

    more test

--- frontend/src/app/services/base.service.spec.ts
+++ frontend/src/app/services/base.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpHeaders } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { BaseService } from './base.service';
 import { environment } from 'src/environments/environment';
@@ -16,5 +17,18 @@ describe('BaseService', () => {
 
 	it('getUrlBase should return base URL', () => {
 		expect(service.getUrlBase()).toEqual(environment.apiUrlBase);
+	});
+	it('httpOptions should be defined correctly', () => {
+		const expectedHttpOptions = {
+			withCredentials: true,
+			headers: new HttpHeaders({
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': '*',
+			}),
+		};
+
+		expect(service.httpOptions.withCredentials).toEqual(expectedHttpOptions.withCredentials);
+		expect(service.httpOptions.headers.get('Content-Type')).toEqual(expectedHttpOptions.headers.get('Content-Type'));
+		expect(service.httpOptions.headers.get('Access-Control-Allow-Origin')).toEqual(expectedHttpOptions.headers.get('Access-Control-Allow-Origin'));
 	});
 });
